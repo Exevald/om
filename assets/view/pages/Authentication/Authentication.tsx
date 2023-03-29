268e00dac54ea84d47a7e9c99da97139db2a3b59
Sergey <100339481+alozhka@users.noreply.github.com>
2023-03-29 18:45:28 +0300

    Почти доверстал логинацию

--- assets/view/pages/Authentication/Authentication.tsx
+++ assets/view/pages/Authentication/Authentication.tsx
@@ -4,13 +4,21 @@ import './Authentication.scss'
 
 
 import InputArea from "../../components/InputArea/InputArea";
+import Button from '../../components/Button/Button'
 
 const Authentication = () => {
     return (
         <div className="auth__main-wrapper">
-            <h2>Войти в Ом</h2>
-            <InputArea header="Электронная почта" type="email" placeholder="Электронная почта"/>
-            <InputArea header="Пароль" type="password" placeholder="Пароль"/>
+            <h2 className="auth__header">
+                Войти в Ом
+            </h2>
+            <InputArea header="Электронная почта" type="email" placeholder="example@example.com"/>
+            <InputArea header="Пароль" type="password" placeholder="****************"/>
+            <div className="auth__registerArea">
+                <p>Нет аккаунта?</p>
+                <a href="">Зарегестрироваться</a>
+            </div>
+            <Button type="submit" submitFor="logination" data="Войти" />
         </div>
     )
 }
