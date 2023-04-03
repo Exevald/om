7675ecc278e51715a776e01f7bb40a23a2c7c5b0
Sergey <100339481+alozhka@users.noreply.github.com>
2023-04-04 00:00:38 +0300

    Написал заглушную ассинхронную ф-ию запроса входа

--- assets/view/pages/Authentication/Authentication.tsx
+++ assets/view/pages/Authentication/Authentication.tsx
@@ -6,8 +6,35 @@ import './Authentication.scss'
 import InputArea from "../../components/InputArea/InputArea";
 import Button from '../../components/Button/Button'
 
+async function postRequest(event: React.SyntheticEvent) {
+    // мб эту строку уберу
+    event.preventDefault();
+
+    const email     = document.getElementById('email') as HTMLInputElement;
+    const pass      = document.getElementById('pass') as HTMLInputElement;
+    const fullName  = document.getElementById('fullName') as HTMLInputElement;
+
+    if (email && pass && fullName) {
+        const data = {
+            email: email.value,
+            pass: pass.value,
+            fullName: fullName.value
+        }
+        const response = await fetch('', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    } else {
+        console.log('Error in getting inputs');
+    }
+}
+
 const Authentication = () => {
     const [register, setRegister] = useState(false);
+    
     return (
         <div className="auth__main-wrapper">
             <h2 className="auth__header">
