1eae50a84be9bc5f2fd80ade5a6fb11004dcdcd8
Sergey <100339481+alozhka@users.noreply.github.com>
2023-04-03 23:59:29 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -64,18 +64,18 @@ Encore
     //.enablePostCssLoader()
 
     // работа с картинками через копирование
-    .copyFiles({
-        from: './assets/view/*/*',
+    // .copyFiles({
+    //     from: './assets/view/*/*',
 
-        // optional target path, relative to the output dir
-        to: 'images/[path][name].[ext]',
+    //     // optional target path, relative to the output dir
+    //     to: 'images/[path][name].[ext]',
 
-        // if versioning is enabled, add the file hash too
-        //to: 'images/[path][name].[hash:8].[ext]',
+    //     // if versioning is enabled, add the file hash too
+    //     //to: 'images/[path][name].[hash:8].[ext]',
 
-        // only copy files matching this pattern
-        pattern: /\.(png|jpg|jpeg|svg)$/
-    })
+    //     // only copy files matching this pattern
+    //     pattern: /\.(png|jpg|jpeg|svg)$/
+    // })
         
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
