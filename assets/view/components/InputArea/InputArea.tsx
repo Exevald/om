35043b64af75449efc8d34f494c4ac44e5661a11
Sergey <100339481+alozhka@users.noreply.github.com>
2023-04-07 14:03:36 +0300

    Убрал ненужное, добавил новые стили

--- assets/view/components/InputArea/InputArea.tsx
+++ assets/view/components/InputArea/InputArea.tsx
@@ -4,36 +4,29 @@ import './InputArea.scss'
 interface InputAreaProps {
     id: string,
     header?: string,
-    type?: 'email' | 'password' | 'submit' | 'text',
-    submitFor?: string
-    placeholder?: string
+    type?: 'email' | 'password' | 'text' | 'group' | 'subject',
+    placeholder?: string,
 }
 
 const InputArea = (props: InputAreaProps) => {
-    if (props.type == 'submit' && props.submitFor)
-        return (
-            <div className="inputArea__wrapper">
-                <input id={props.id} className="inputArea__input" type={props.type} />
-            </div>
-        )
-    else 
-        return (
-            <div className="inputArea__wrapper">
-                {
-                    props.header &&
-                    <div id={props.id} className="inputArea__header">
-                        <p>{props.header}</p>
-                    </div>
-                }
-                
-                {
-                    props.type && 
-                    <input id={props.id} className="inputArea__input" 
-                    type={props.type} placeholder={props.placeholder}/>
-                }
-                        
-            </div>
-        )
+    const styles = `inputArea__input inputArea__${props.type}`
+    return (
+        <div>
+            {
+                props.header &&
+                <div className="inputArea__header">
+                    <p>{props.header}</p>
+                </div>
+            }
+            
+            {
+                props.type && 
+                <input id={props.id} className={styles}
+                type={props.type} placeholder={props.placeholder}/>
+            }
+                    
+        </div>
+    )
 }
 
 export default InputArea;
