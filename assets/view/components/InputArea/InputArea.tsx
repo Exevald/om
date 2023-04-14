199f4183561e97c850a220ec5f40aada900112db
Sergey <100339481+alozhka@users.noreply.github.com>
2023-04-14 23:42:26 +0300

    Добавил адаптивное изменение длины

--- assets/view/components/InputArea/InputArea.tsx
+++ assets/view/components/InputArea/InputArea.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './InputArea.scss'
+import { MIN_LABEL_GROUP_SIZE } from "../../../utility/utilities";
 
 interface InputAreaProps {
     id: string,
     header?: string,
-    type?: 'email' | 'password' | 'text' | 'group' | 'subject',
+    type: 'email' | 'password' | 'text' | 'group' | 'subject',
     placeholder?: string,
     value?: string
 }
+function changeWidth(id: string) {
+    const el = document.getElementById(id) as HTMLInputElement;
+    if (el.value.length < MIN_LABEL_GROUP_SIZE) {
+        el.style.width =  MIN_LABEL_GROUP_SIZE + 'ch';
+    } else {
+        el.style.width = el.value.length + 'ch';
+    }
+}
 
 const InputArea = (props: InputAreaProps) => {
-    const styles = `inputArea__input inputArea__${props.type}`
+    const styles = `inputArea__input inputArea__${props.type}`;
+
+    if (props.id in ['name', 'subject']) {
+        const onChange = changeWidth
+    } else {
+        const onChange = ''
+    }
+
+    useEffect(() => changeWidth(props.id))
     return (
         <div>
             {
@@ -22,8 +39,9 @@ const InputArea = (props: InputAreaProps) => {
             
             {
                 props.type && 
-                <input id={props.id} className={styles} defaultValue={props.value}
-                type={props.type} placeholder={props.placeholder}  />
+                <input id={props.id} className={styles} defaultValue={props.value} type={props.type} 
+                onChange={() => changeWidth(props.id)} 
+                placeholder={props.placeholder} />
             }
             
             {
