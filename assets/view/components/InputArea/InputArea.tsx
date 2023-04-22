c15052ea6fecb404510257100791d5aeeeb6b14a
Sergey <100339481+alozhka@users.noreply.github.com>
2023-04-23 00:02:39 +0300

    Добавил проп, отвечающий за изменеине длины input

--- assets/view/components/InputArea/InputArea.tsx
+++ assets/view/components/InputArea/InputArea.tsx
@@ -4,27 +4,27 @@ import { MIN_LABEL_GROUP_SIZE } from "../../../utility/utilities";
 
 interface InputAreaProps {
     id: string,
+    type: 'email' | 'password' | 'text' | 'group' | 'subject' | 'checkbox' | 'studentName' | 'studentSurname',
     header?: string,
-    type: 'email' | 'password' | 'text' | 'group' | 'subject' | 'checkbox',
+    widthChangeable?: boolean,
     placeholder?: string,
     value?: string
 }
-function changeWidth(id: string, type: InputAreaProps['type']) {
-    if (type === 'group' || type === 'subject') {
-        const el = document.getElementById(id) as HTMLInputElement;
-        if (el.value.length < MIN_LABEL_GROUP_SIZE) {
-            el.style.width =  MIN_LABEL_GROUP_SIZE + 'ch';
-        } else {
-            el.style.width = el.value.length + 'ch';
-        }
+function changeWidth(id: string) {
+    const el = document.getElementById(id) as HTMLInputElement;
+    if (el.value.length < MIN_LABEL_GROUP_SIZE) {
+        el.style.width =  MIN_LABEL_GROUP_SIZE + 'ch';
+    } else {
+        el.style.width = el.value.length + 'ch';
     }
 }
 
 const InputArea = (props: InputAreaProps) => {
     const styles = `inputArea__input inputArea__${props.type}`;
 
-
-    useEffect(() => changeWidth(props.id, props.type))
+    if (props.widthChangeable) {
+        useEffect(() => changeWidth(props.id))
+    }
     return (
         <div>
             {
@@ -35,9 +35,15 @@ const InputArea = (props: InputAreaProps) => {
             }
             
             {
-                props.type && 
+                props.widthChangeable && 
                 <input id={props.id} className={styles} defaultValue={props.value} type={props.type} 
-                onChange={() => changeWidth(props.id, props.type)} 
+                onChange={() => changeWidth(props.id)} 
+                placeholder={props.placeholder} />
+            }
+
+            {
+                !props.widthChangeable && 
+                <input id={props.id} className={styles} defaultValue={props.value} type={props.type} 
                 placeholder={props.placeholder} />
             }
                     
