3de8d6487c67782a5bd8b936544bd3348799ed47
Sergey <100339481+alozhka@users.noreply.github.com>
2023-05-13 21:41:35 +0300

    В процессе вёрсти таблицы

--- assets/view/components/Table/Table.tsx
+++ assets/view/components/Table/Table.tsx
@@ -1,43 +1,86 @@
-import React, { useState } from "react";
+import React from "react";
 import {  Group, Student, Task } from "../../../utility/types";
 
 import './Table.scss'
 
 
-interface TableHeaderProps {
+interface StudentTableProps {
     subject: string,
-    tasks: Array<Task>
+    students: Array<Student>
 }
-const TableHeader = (props: TableHeaderProps) => {
+const StudentsTable = (props: StudentTableProps) => {
     return (
-        <thead className="table__head">
-            <tr>
-                <th>{props.subject}</th>
+        <table className="table__wrapper">
+            <thead><tr><td>{props.subject}</td></tr></thead>
+            <tbody>
                 {
-                    props.tasks.map(task =>
-                        <th key={task.id}>{task.topic}</th>    
+                    props.students.map(student => 
+                        <tr key={student.id}>
+                            <td>
+                                {student.firstName} {student.lastName}
+                            </td>
+                        </tr>
                     )
                 }
-                <th>Итого</th>
-            </tr>
-        </thead>
+                    <tr><td>󠇮</td></tr>
+            </tbody>
+        </table>
     )
 }
 
-interface TableRowProps {
-    student: Student,
-    tasks: Array<Task>
+interface TasksTableProps {
+    tasks: Array<Task>,
+    countOfRows: number
 }
-const TableRow = (props: TableRowProps) => {
+const TasksTable = (props: TasksTableProps) => {
+    const tasksHead = props.tasks.map(task =>
+        <th key={task.id}>{task.id}</th>
+    )
+    tasksHead.push(
+        <th key={-1}></th>
+    )
+    const tasksRows = props.tasks.map(task => <tr></tr>)
     return (
-        <tr>
-            <td>
-                {props.student.firstName} {props.student.lastName}
-            </td>
+        <table className="table__wrapper table__tasks">
+            <thead>
+                <tr>{tasksHead}</tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
+
+
+interface FinalMarksTableProps {
+    tasks: Array<Task>,
+    countOfRows: number
+}
+const FinalMarksTable = (props: FinalMarksTableProps) => {
+    let finalMarks: Array<JSX.Element> = []
+    for (let i = 0; i < props.countOfRows; i++) {
+        // здесь рендер финальной оценки
+        finalMarks.push(
+            <tr key={i}>
+                <td>{i}</td>
+            </tr>
+        )
+    }
+    finalMarks.push(
+        <tr key={finalMarks.length + 1}>
+            <td>100</td>
         </tr>
     )
+    return (
+        <table className="table__wrapper">
+            <thead><tr><td><strong>Итого</strong></td></tr></thead>
+            <tbody>
+                {finalMarks}
+            </tbody>
+        </table>
+    )
 }
-
 
 
 interface TableProps {
@@ -46,16 +89,12 @@ interface TableProps {
     tasks: Array<Task>
 }
 const Table = (props: TableProps) => {
-    const rows = props.students.map(student => 
-        <TableRow key={student.id} student={student} tasks={props.tasks}/>
-    )
     return(
-        <table className="table__wrapper">
-            <TableHeader subject={props.subject} tasks={props.tasks}/>
-            <tbody className="table__body">
-                {rows}
-            </tbody>
-        </table>
+        <div className="table__tables">
+            <StudentsTable subject={props.subject} students={props.students}/>
+            <TasksTable tasks={props.tasks} countOfRows={props.students.length}/>
+            <FinalMarksTable tasks={props.tasks} countOfRows={props.students.length}/>
+        </div>
     )
 }
 
