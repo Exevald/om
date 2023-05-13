10afb09c1e85f692b5c6038e7f2509936785559e
Sergey <100339481+alozhka@users.noreply.github.com>
2023-05-13 21:52:29 +0300

    Начал писать компонент заглавия задания

---
+++ assets/view/components/TaskLabel/TaskLabel.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// @ts-ignore
+import taskIcon from './taskIcon.svg'
+
+
+interface TaskLabelProps {
+    date: Date,
+    onClick: () => void
+}
+const TaskLabel = () => {
+    <div></div>
+}
+
+
+export default TaskLabel
